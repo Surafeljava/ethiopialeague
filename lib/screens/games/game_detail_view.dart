9510416bf91128a36aa5335cb18b06dd93bee929
ODC-ETHIOPIA <surafelk27@gmail.com>
2021-07-02 17:22:39 +0300

    feat: Game Display pop up done

--- lib/screens/games/game_detail_view.dart
+++ lib/screens/games/game_detail_view.dart
@@ -1,10 +1,100 @@
+import 'package:ethioleague/models/game.dart';
 import 'package:flutter/material.dart';
 
 class GameDetailView extends StatelessWidget {
-  const GameDetailView({Key? key}) : super(key: key);
+  final Game game;
+  const GameDetailView({Key? key, required this.game}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 100,
+      margin: EdgeInsets.all(4),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(5.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            offset: Offset(1, 2),
+            blurRadius: 6.0,
+          )
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                game.home_team.full_name,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 15.0,
+                ),
+              ),
+              Text(
+                game.home_team.abbreviation,
+                style: TextStyle(
+                  color: Colors.grey[600],
+                  fontWeight: FontWeight.w400,
+                  fontSize: 13.0,
+                ),
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                "${game.date.day}-${game.date.month}-${game.date.year}",
+                style: TextStyle(
+                  color: Colors.grey[500],
+                  fontWeight: FontWeight.w500,
+                  fontSize: 14.0,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(4.0),
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(3),
+                ),
+                child: Text(
+                  "${game.home_team_score} - ${game.visitor_team_score}",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w500),
+                ),
+              ),
+              Text("${game.time}"),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                game.visitor_team.full_name,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 15.0,
+                ),
+              ),
+              Text(
+                game.visitor_team.abbreviation,
+                style: TextStyle(
+                  color: Colors.grey[600],
+                  fontWeight: FontWeight.w400,
+                  fontSize: 13.0,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
